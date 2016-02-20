76040cc8fd87cac39188166dc0af0d3e255ae711
Brian Kemper <bkemper@gmail.com>
2016-02-20 10:18:28 -0700

    added default ng-options value, moved pagination to service, combined get servers

--- controller.js
+++ controller.js
@@ -1,30 +1,18 @@
 angular.module('app').controller('mainController', function($scope, svc){
 
-$scope.selected = $scope.things;
 
-  $scope.getSearch = function(){
-    svc.search($scope.selected())
+
+  $scope.getSearch = function(x){
+    svc.search(x)
     .then(function(response){
-      $scope.searchData = response.data.Search;
-      $scope.param = response.config.params;
-      $scope.page = [];
-        for (j=2; j<=Math.round(response.data.totalResults/10); j++) {
-          $scope.param.page = j;
-          var objClone = JSON.parse(JSON.stringify($scope.param));
-          $scope.page.push(objClone);
-        }
+      $scope.searchData = response[0];
+      $scope.page = response[1];
     });
   };
 
-    $scope.getPages = function(p){
-      console.log('hi' + p);
-      svc.search(p)
-      .then(function(response){
-        $scope.searchData = response.data.Search;
-      });
-    };
-
   $scope.things = [
+    {display: 'All',
+    val: function(){return {s: $scope.title};}},
   {display: 'Movie',
   val: function(){return {type: 'movie', s: $scope.title};}},
     {display: 'Series',
@@ -33,37 +21,6 @@ $scope.selected = $scope.things;
   val: function(){return {type: 'episode', s: $scope.title};}},
 ];
 
-// we know we have Math.round(searchData.totalResults/10) pages
-// we want an array of objects that link to the api request wanted
+$scope.selected = $scope.things[0].val;
 
 });
-
-
-
-
-// Poster: "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg"
-// Title: "Batman Begins"
-// Type: "movie"
-// Year: "2005"
-// imdbID: "tt0372784"
-//
-// Actors: "Michael Keaton, Jack Nicholson, Kim Basinger, Robert Wuhl"
-// Awards: "Won 1 Oscar. Another 9 wins & 22 nominations."
-// Country: "USA, UK"
-// Director: "Tim Burton"
-// Genre: "Action, Adventure"
-// Language: "English, French"
-// Metascore: "66"
-// Plot: "The Dark Knight of Gotham City begins his war on crime with his first major enemy being the clownishly homicidal Joker."
-// Poster: "http://ia.media-imdb.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg"
-// Rated: "PG-13"
-// Released: "23 Jun 1989"
-// Response: "True"
-// Runtime: "126 min"
-// Title: "Batman"
-// Type: "movie"
-// Writer: "Bob Kane (Batman characters), Sam Hamm (story), Sam Hamm (screenplay), Warren Skaaren (screenplay)"
-// Year: "1989"
-// imdbID: "tt0096895"
-// imdbRating: "7.6"
-// imdbVotes: "256,000"
--- service.js
+++ service.js
@@ -6,36 +6,16 @@ angular.module('app').service('svc', function($http){
         method: "GET",
         params: paramConf,
         }).then(function(response) {
-          return response;
+          var param = response.config.params;
+          var page = [];
+            for (i=2; i<=Math.round(response.data.totalResults/10); i++) {
+              param.page = i;
+              var objClone = JSON.parse(JSON.stringify(param));
+              page.push(objClone);
+            }
+          var returnArr = [response.data.Search, page];
+          return returnArr;
       });
   };
 
 });
-
-
-// Poster: "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg"
-// Title: "Batman Begins"
-// Type: "movie"
-// Year: "2005"
-// imdbID: "tt0372784"
-
-// Actors: "Michael Keaton, Jack Nicholson, Kim Basinger, Robert Wuhl"
-// Awards: "Won 1 Oscar. Another 9 wins & 22 nominations."
-// Country: "USA, UK"
-// Director: "Tim Burton"
-// Genre: "Action, Adventure"
-// Language: "English, French"
-// Metascore: "66"
-// Plot: "The Dark Knight of Gotham City begins his war on crime with his first major enemy being the clownishly homicidal Joker."
-// Poster: "http://ia.media-imdb.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg"
-// Rated: "PG-13"
-// Released: "23 Jun 1989"
-// Response: "True"
-// Runtime: "126 min"
-// Title: "Batman"
-// Type: "movie"
-// Writer: "Bob Kane (Batman characters), Sam Hamm (story), Sam Hamm (screenplay), Warren Skaaren (screenplay)"
-// Year: "1989"
-// imdbID: "tt0096895"
-// imdbRating: "7.6"
-// imdbVotes: "256,000"
